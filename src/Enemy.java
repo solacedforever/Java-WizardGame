12759efb8df5dff15caebf95c77e82708bc0ffd4
James Spargo <solacedforever@gmail.com>
2017-11-28 18:46:26 -0500

    added more logic to prevent enemies from running into walls, also added the logic for them to be destroyed from bullets

--- src/Enemy.java
+++ src/Enemy.java
@@ -19,19 +19,48 @@ public class Enemy extends GameObject {
         
         choose = r.nextInt(10);
         
-        if(choose == 0) {
-            velX = (r.nextInt(4 - -4 + -4));
-            velY = (r.nextInt(4 - -4 + -4));
+        for(int i = 0; i < handler.object.size(); i++) {
+            GameObject tempObject = handler.object.get(i);
+            
+            if(tempObject.getId() == ID.Block) {
+                if(getBoundsBig().intersects(tempObject.getBounds())) {
+                    x += (velX*5) * -1;
+                    y += (velY*2) * -1;
+                    velX *= -1;
+                    velY *= -1;
+                }else if(choose == 0) {
+                    velX = (r.nextInt(4 - -4 + -4));
+                    velY = (r.nextInt(4 - -4 + -4));
+                }
+            }
+            
+            if(tempObject.getId() == ID.Bullet) {
+                if(getBounds().intersects(tempObject.getBounds())) {
+                    hp -= 50;
+                    handler.removeObject(tempObject);
+                }
+               
+            }
         }
+        
+        if(hp <= 0) handler.removeObject(this);
+        
+     
     }
     
     public void render(Graphics g) {
         g.setColor(Color.yellow);
         g.fillRect(x,y,32,32);
         
+        Graphics2D g2d = (Graphics2D) g;
+ 
     }
     
     public Rectangle getBounds() {
         return new Rectangle(x,y,32,32);
+    }
+    
+    public Rectangle getBoundsBig() {
+        return new Rectangle(x-16,y-16,64,64);
     }
 }
