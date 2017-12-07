8ca4307934aa6c3b6976707bb2371bb48f6a2a4c
James Spargo <solacedforever@gmail.com>
2017-12-07 16:25:45 -0500

    added another level file and sprite sheet for character and enemy images, creating SpriteSheet class

---
+++ src/SpriteSheet.java
@@ -0,0 +1,13 @@
+import java.awt.image.BufferedImage;
+
+public class SpriteSheet {
+    
+    private BufferedImage image;
+    
+    public SpriteSheet(BufferedImage image) {
+        this.image = image;
+    }
+    
+    
+    
+}
