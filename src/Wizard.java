fa73699d44e8015093f9be5478b108395bfb775d
James Spargo <solacedforever@gmail.com>
2018-01-17 16:56:57 -0500

    added array for the sprite animations in wizard class along with animation logic

--- src/Wizard.java
+++ src/Wizard.java
@@ -1,14 +1,25 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Wizard extends GameObject {
     
     Handler handler;
     Game game;
     
+    private BufferedImage[] wizard_image = new BufferedImage[3];
+    
+    Animation anim;
+    
     public Wizard(int x, int y, ID id, Handler handler, Game game, SpriteSheet ss) {
         super(x, y, id, ss);
         this.handler = handler;
         this.game = game;
+        
+        wizard_image[0] = ss.grabImage(1, 1,32,48);
+        wizard_image[1] = ss.grabImage(2, 1,32,48);
+        wizard_image[2] = ss.grabImage(3, 1,32,48);
+        
+        anim = new Animation(3,wizard_image[0], wizard_image[1], wizard_image[2]);
     }
     
     public void tick() {
@@ -32,6 +43,9 @@ public class Wizard extends GameObject {
     
         if(handler.isLeft()) velX = -5;
         else if (!handler.isRight()) velX = 0;
+        
+        anim.runAnimation();
+        
     }
     
     private void collision() {
@@ -62,8 +76,11 @@ public class Wizard extends GameObject {
     }
     
     public void render(Graphics g) {
-        g.setColor(Color.blue);
-        g.fillRect(x, y,32,48);
+       if(velX == 0 && velY == 0)
+           g.drawImage(wizard_image[0],x,y,null);
+        else
+            anim.drawAnimation(g,x,y,0);
+        
         
     }
     
